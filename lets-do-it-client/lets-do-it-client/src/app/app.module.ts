bbe6266d9c761c4b5a261f9d7ffe699c9d2b85df
aviram zuzut <aviramzuzut@gmail.com>
2020-11-19 18:24:27 +0200

    WIP - add MatButtonModule, now we can add material components...

--- lets-do-it-client/lets-do-it-client/src/app/app.module.ts
+++ lets-do-it-client/lets-do-it-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    CommonModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
