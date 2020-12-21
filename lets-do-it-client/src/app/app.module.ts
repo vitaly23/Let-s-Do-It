777ebac1bf8c93f739ef1c61f2c5467ac1820c1a
aviram zuzut <aviramzuzut@gmail.com>
2020-12-21 22:43:41 +0200

    Fix project tree for client. Downgrade Angular to 10.0.0

--- lets-do-it-client/src/app/app.module.ts
+++ lets-do-it-client/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    MatButtonModule
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
