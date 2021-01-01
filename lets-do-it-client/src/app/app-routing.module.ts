cced929884c6338f0cb0f1a4c4a419512a4e3dd2
aviram zuzut <aviramzuzut@gmail.com>
2021-01-01 12:52:26 +0200

    WIP

--- lets-do-it-client/src/app/app-routing.module.ts
+++ lets-do-it-client/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './homepage/login/login.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path:'homepage', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 @NgModule({
