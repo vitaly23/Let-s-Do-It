f6294fc22ab75ac46426c44f37be16858d993c23
bonbono <benbonbon@gmail.com>
2021-01-09 14:38:32 +0200

    added meeting sub components and changed map component

--- lets-do-it-client/src/app/homepage/map/map.component.ts
+++ lets-do-it-client/src/app/homepage/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Meeting } from 'src/app/core/services/meeting-information/meeting';
 import { MeetingService } from 'src/app/core/services/meeting-information/meeting.service';
@@ -11,17 +11,19 @@ import { MeetingService } from 'src/app/core/services/meeting-information/meetin
 export class MapComponent implements OnInit {
 
   public meetings: Meeting[];
+  @Input() height: number = 180;
+  @Input() width: number = 180;
+  @Input() zoom: number = 10;
+  @Input() lat: number = 32.109333;
+  @Input() lng: number = 34.855499;
 
-  lat = 32.109333;
-  lng = 34.855499;
-  zoom=10;
+  constructor(private _meetingService: MeetingService) {
 
-  constructor(private _meetingService: MeetingService) { 
-    
   }
 
   ngOnInit(): void {
-    this._meetingService.getMeetings().subscribe(meetings=>{this.meetings=meetings;});
+    this._meetingService.getMeetings().subscribe(meetings => { this.meetings = meetings; });
+    console.log('width ' + this.width + ' height ' + this.height);
   }
 
 }
