42c5804b3ece2ade571c07372c51a94706b7d235
aviram zuzut <aviramzuzut@gmail.com>
2021-01-09 17:55:06 +0200

    Allow origin for client

--- src/main/java/dts/Application.java
+++ src/main/java/dts/Application.java
@@ -19,8 +19,7 @@ public class Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/greeting-javaconfig")
-				.allowedOrigins("http://localhost:8080")
-				.allowedOrigins("http://localhost:8081");
+				.allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
