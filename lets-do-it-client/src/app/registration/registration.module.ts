6aaa72f475c55b0844ad5ad7843af9a473d2af8a
aviram zuzut <aviramzuzut@gmail.com>
2021-01-09 13:43:57 +0200

    WIP

--- lets-do-it-client/src/app/registration/registration.module.ts
+++ lets-do-it-client/src/app/registration/registration.module.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { RegistrationComponent } from './registration.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -15,7 +16,8 @@ import { RegistrationComponent } from './registration.component';
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule
   ],
-  exports:[RegistrationComponent]
+  exports: [RegistrationComponent]
 })
 export class RegistrationModule { }
